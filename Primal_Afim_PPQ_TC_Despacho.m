b9c3d056c5cdf2eb844523bf7a0da753830ee31d
Rafael Pavan <50079320+rafaelpavan95@users.noreply.github.com>
2021-05-30 21:16:53 -0300

    Update Primal_Afim_PPQ_TC_Despacho.m

--- Primal_Afim_PPQ_TC_Despacho.m
+++ Primal_Afim_PPQ_TC_Despacho.m
@@ -174,7 +174,7 @@ while(iteracoes<max)
 
      % Cálculo do Comprimento do Passo:
 
-    alfak2 = (-transpose(dxk)*(Q*xk+c))/(transpose(dxk)*Q*dxk);
+    alfak2 = (-transpose(dxk)*(Q*xk+c-uk*inv(Xk)*transpose(e)))/(transpose(dxk)*(Q + (uk+1)*inv(Xk)*inv(Xk))*dxk);
 
     alfak1 = min(-alfa*xk(dxk<0)./dxk(dxk<0));
 
